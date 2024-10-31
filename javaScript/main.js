689e04cf0048dc117f355fe9fb3d230f42a19ba8
Kaviru <kavirunethsara1@outlook.com>
2024-10-31 22:13:36 +0530

    function the  addtolist

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,8 +1,15 @@
 /* <div title="Hit Double Click and Complete" ondblclick="toDoDone()">Sample One</div>
 <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/> */
 
+let list = document.getElementById('list');
 
 function addToList(){
+    const userInput = document.getElementById('inputText').value;
+    let li = document.createElement('li');
+    li.innerHTML += `<div title="Hit Double Click and Complete" ondblclick="toDoDone()">${userInput}</div>
+                    <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/>`
+    
+    list.appendChild(li);
     
 }
 
