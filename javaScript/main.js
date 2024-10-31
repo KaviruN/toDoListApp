c2a026940efd5dddfb11f4aec74152796b7eec49
Kaviru <kavirunethsara1@outlook.com>
2024-10-31 21:58:04 +0530

    add done-style class for completed tasks; enhance visual representation

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,2 +1,4 @@
 /* <div title="Hit Double Click and Complete" ondblclick="toDoDone()">Sample One</div>
 <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/> */
+
+
