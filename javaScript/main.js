8bbb6e944e0617b5c6a59527fe2a7d24784f9b91
Kaviru <kavirunethsara1@outlook.com>
2024-10-31 22:30:36 +0530

    add if to cheack user input errors

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,19 +2,30 @@
 <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/> */
 
 let list = document.getElementById('list');
+const alertMg = document.getElementById('alert');
 
-function addToList(){
+function addToList() {
     const userInput = document.getElementById('inputText').value;
     let li = document.createElement('li');
-    li.innerHTML += `<div title="Hit Double Click and Complete" ondblclick="toDoDone()">${userInput}</div>
-                    <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/>`
     
-    list.appendChild(li);
-    
+    if(userInput == ''){
+        alertMg.style.color = 'red';
+        alertMg.textContent = 'Plese Add Item!';
+    }
+    else {
+        li.innerHTML += `<div title="Hit Double Click and Complete" ondblclick="toDoDone()">${userInput}</div>
+        <img src="images/delete.svg" alt="Delete" class="delete-icon" id="delete-img" onclick="toDoDelete()"/>`
+
+        list.appendChild(li);
+        alertMg.style.color = 'rgb(15, 197, 15)'
+        alertMg.textContent = 'To Do item Crated Succeslly! '
+        setInterval( () => alertMg.textContent = '',3000);
+    }
+
 }
 
 function toDoDone() {
 
 }
 
-function toDoDelete(){}
+function toDoDelete() { }
